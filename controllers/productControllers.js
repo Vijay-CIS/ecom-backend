d3afe6c0f952cc45e8c85d4c80a4679d05fc75d6
Vijay <vijay.annamalai001@gmail.com>
2026-06-10 15:13:17 +0530

    search feature

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,24 +1,31 @@
-
-const productModel = require('../models/productModel');
+const productModel = require("../models/productModel");
 
 //Get all products => /api/v1/products
 
-exports.getProducts = async  (req, res, next) => {
-    try {
-        const products = await productModel.find();
-        res.json({ success: true, products, message: 'Get all products' });
-    } catch (err) {
-        res.status(500).json({ success: false, message: 'Server Error' });
-    }
-}
+exports.getProducts = async (req, res, next) => {
+  try {
+    const query = req.query.keyword
+      ? {
+          name: {
+            $regex: req.query.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    const products = await productModel.find(query);
+    res.json({ success: true, products, message: "Get all products" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
 
 //Get product by ID => /api/v1/product/:id
 
 exports.getProductById = async (req, res, next) => {
-    try {
-        const product = await productModel.findById(req.params.id);
-        res.json({ success: true, product, message: 'Get product by ID' });
-    } catch (err) {
-        res.status(500).json({ success: false, message: 'Server Error' });
-    }
-}
+  try {
+    const product = await productModel.findById(req.params.id);
+    res.json({ success: true, product, message: "Get product by ID" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
